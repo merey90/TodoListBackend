871ed71f8341db0d4946de0c4f0bf6d91cf37125
Merey Zholdas <merey.zholdas@deliveryhero.com>
2020-07-22 00:27:58 +0200

    logging error

--- src/routes/Users.ts
+++ src/routes/Users.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from 'express';
-import { CREATED, OK } from 'http-status-codes';
+import { CREATED, OK, BAD_REQUEST } from 'http-status-codes';
 import { ParamsDictionary } from 'express-serve-static-core';
 
 import { UserModel } from '@daos/User';
@@ -7,8 +7,13 @@ import { UserModel } from '@daos/User';
 const router = Router();
 
 router.get('/', async (req: Request, res: Response) => {
-  const users = await UserModel.find({});
-  res.status(OK).json(users);
+  try {
+    const users = await UserModel.find({});
+    res.status(OK).json(users);
+  } catch (error) {
+    console.log('error: ', error);
+    res.status(BAD_REQUEST).json({ error });
+  }
 });
 
 router.get('/:id', async (req: Request, res: Response) => {

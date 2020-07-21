0a65c89689f77a2724fc2f64d7a7eff7a3a5ee07
Merey Zholdas <merey.zholdas@deliveryhero.com>
2020-07-21 23:58:51 +0200

    heroku env variables

--- src/LoadEnv.ts
+++ src/LoadEnv.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 
 // Set the env file
-const configs = dotenv.config();
+const configs = dotenv.config({ path: __dirname + '/.env' });
 
 if (configs.error) {
   throw configs.error;

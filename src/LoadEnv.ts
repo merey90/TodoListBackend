7e85f19a4a7047131ba452cad852cf2fa1fd6de4
Merey Zholdas <merey.zholdas@deliveryhero.com>
2020-07-22 00:01:20 +0200

    envs

--- src/LoadEnv.ts
+++ src/LoadEnv.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 
 // Set the env file
-const configs = dotenv.config({ path: __dirname + '/.env' });
+const configs = dotenv.config({ path: '/.env' });
 
 if (configs.error) {
   throw configs.error;
